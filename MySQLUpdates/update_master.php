01d3dfd8ef851792a636fb755ac8430fc808423b
Bentley <bentley.hudson@henryschein.com>
2019-04-18 07:30:35 -0500

    replen autoupdate

--- MySQLUpdates/update_master.php
+++ MySQLUpdates/update_master.php
@@ -3,14 +3,17 @@
 //master to update all slotting recommendations
 //need to add logic to scan FTP folder for new files and add if any are there
 
-//include 'tableupdate_slotmaster.php';
-//add other update tables here
+include 'tableupdate_slotmaster.php';
+include 'tableupdate_itemmaster.php';
+include 'tableupdate_replen.php';
+include 'tableupdate_sales.php';
+include 'tableupdate_locmaster.php';
+include 'current_implied_moves.php';  //has to be called after tableupdate_slotmaster.php
 
 include 'DemandGrouping.php';
 
 //Should no sales update be placed here?
 
-include 'current_implied_moves.php';  //has to be called after tableupdate_slotmaster.php
 include 'itemtruefits.php';
 include 'replenpercube.php';
 include'NPFMVC_Update.php';
