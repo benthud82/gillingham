e8a2073ceb4dd88fbed342de0d5e7bd655777e5a
Bentley <bentley.hudson@henryschein.com>
2019-05-13 14:34:12 -0500

    conveyable fix

--- MySQLUpdates/update_master.php
+++ MySQLUpdates/update_master.php
@@ -9,11 +9,10 @@ include 'tableupdate_replen.php';
 include 'tableupdate_sales.php';
 include 'tableupdate_locmaster.php';
 include 'current_implied_moves.php';  //has to be called after tableupdate_slotmaster.php
-
 include 'DemandGrouping.php';
-
 include 'itemtruefits.php';
 include 'replenpercube.php';
 include'NPFMVC_Update.php';
 include'optimalbayloose.php';
+include'itemscore.php';
 
