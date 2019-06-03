34bdc08254f0965765d579d6227cfde000e9f1e9
Bentley <bentley.hudson@henryschein.com>
2019-06-03 10:54:10 -0500

    endcap fix

---
+++ globaldata/modal_grid5.php
@@ -0,0 +1,19 @@
+
+<!-- grid5 dimension modal -->
+<div id="modal_grid5dim" class="modal fade " role="dialog">
+    <div class="modal-dialog modal-lg">
+
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Location Dimensions</h4>
+            </div>
+
+            <div class="modal-body" id="" style="margin: 25px;">
+                <div id="locdimdata"></div>
+            </div>
+
+        </div>
+    </div>
+</div>
