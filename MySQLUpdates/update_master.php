19b311167e7855ae5ccd5eb86d5c28e6ec4995ba
Bentley <bentley.hudson@henryschein.com>
2019-06-03 13:49:16 -0500

    update sleep

--- MySQLUpdates/update_master.php
+++ MySQLUpdates/update_master.php
@@ -3,16 +3,28 @@
 //master to update all slotting recommendations
 //need to add logic to scan FTP folder for new files and add if any are there
 include 'tableupdate_locmaster.php';  //must update before slotmaster because tier is pulled from here for slotmaster
+sleep(10);
 include 'tableupdate_slotmaster.php';
+sleep(10);
 include 'tableupdate_itemmaster.php';
+sleep(10);
 include 'tableupdate_replen.php';
+sleep(10);
 include 'tableupdate_sales.php';
+sleep(10);
 include 'current_implied_moves.php';  //has to be called after tableupdate_slotmaster.php
+sleep(10);
 include 'DemandGrouping.php';
+sleep(10);
 include 'itemtruefits_ecap.php';
+sleep(10);
 include 'itemtruefits.php';
+sleep(10);
 include 'replenpercube.php';
+sleep(10);
 include'NPFMVC_Update.php';
+sleep(10);
 include'optimalbayloose.php';
+sleep(10);
 include'itemscore.php';
 
